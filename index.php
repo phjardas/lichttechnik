50ee70b7215c029db85419d56e3a2bcc083d54f1
Philipp Jardas <philipp@jardas.de>
2017-08-07 21:09:49 +0200

    Apply HSTS and CSP

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?
+  header('Strict-Transport-Security: max-age=10886400');
+  header('Content-Security-Policy: upgrade-insecure-requests');
+
+  if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off") {
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit();
+  }
+
   function __autoload($class_name) {
     require_once 'lib/' . $class_name . '.php';
   }
